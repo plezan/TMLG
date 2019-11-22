251ed33e4fc8d21783a45c72bb17d83fe6d17906
Pierre LEZAN <1pierrelezan@gmail.com>
2019-11-23 00:50:33 +0100

    minimal player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public float jump;
+    public float rayCheckLength;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+
+        // horizontal movement
+        float x = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector3(x * speed, rb.velocity.y, 0);
+
+        // vertical movement
+        if (Input.GetAxis("Jump") > 0 && isGrounded())
+        {
+            rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+        }
+    }
+
+    bool isGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, rayCheckLength);
+        return hit.collider != null;
+    }
+
+}
