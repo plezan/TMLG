d5f8dcf688d4f4942c88b77d84cbaa6554ce26d1
RIOU Kévin <riou.kkevin@outlook.com>
2019-11-23 10:51:48 +0100

    First release of platform generator

---
+++ Assets/Scripts/plateGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plateGenerator : MonoBehaviour
+{
+    public GameObject _self;
+    public GameObject plateSample;
+    public string plateName;
+    public int minX;
+    public int maxX;
+    public int minY;
+    public int maxY;
+    public int plateCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < plateCount; i++)
+        {
+            clone();
+        }
+    }
+    GameObject clone()
+    {
+        var clone = Instantiate(plateSample);
+        clone.transform.SetParent(_self.transform);
+        clone.name = plateName;
+        int x = Random.Range(minX, maxX) + (int) _self.transform.position.x;
+        int y = Random.Range(minY, maxY);
+        float z = -0.5f;
+        clone.transform.position = new Vector3(x, y, z);
+        return clone;
+    }
+}
