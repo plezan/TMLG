c0072f91e366143a4c51f1ca7dee2e48bdf8b567
Pierre LEZAN <1pierrelezan@gmail.com>
2019-11-23 10:43:35 +0100

    semi-scalable multiplayer

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -7,21 +7,19 @@ public class SpawnController : MonoBehaviour
 
     public int nbPlayer = 2;
     public GameObject playerPrefab;
-    public Transform Spawn1;
-    public Transform Spawn2;
-
-    public GameObject[] players;
+    public Transform[] spawns;
+    GameObject[] players;
 
     // Start is called before the first frame update
     void Start()
     {
-        players[0] = Instantiate(playerPrefab, Spawn1);
-        players[1] = Instantiate(playerPrefab, Spawn2);
-        PlayerController playerController1 = players[0].GetComponent<PlayerController>();
-        PlayerController playerController2 = players[1].GetComponent<PlayerController>();
-        playerController1.HorizontalAxis = "p1_Horizontal";
-        playerController1.VerticalAxis = "p1_Vertical";
-        playerController2.HorizontalAxis = "p2_Horizontal";
-        playerController2.VerticalAxis = "p2_Vertical";
+        players = new GameObject[nbPlayer];
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i] = Instantiate(playerPrefab, spawns[i]);
+            PlayerController pc = players[i].GetComponent<PlayerController>();
+            pc.HorizontalAxis = "p"+(i+1)+"_Horizontal";
+            pc.VerticalAxis = "p"+(i+1)+"_Vertical";
+        }
     }
 }
