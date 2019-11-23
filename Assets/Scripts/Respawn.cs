21ca79a4b41fe737f158ba82e990564ae26cd1f6
RIOU Kévin <riou.kkevin@outlook.com>
2019-11-23 15:29:34 +0100

    add respawn

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject SelectedPlayer = other.gameObject;
+        Debug.Log(SelectedPlayer.name);
+        if (other.name.Contains("Player"))
+        {
+            float x = SelectedPlayer.transform.position.x;
+            float y = 22;
+            float z = SelectedPlayer.transform.position.z;
+            SelectedPlayer.transform.SetPositionAndRotation(new Vector3(x, y, z), new Quaternion(0,0,0,0));
+        }
+    }
+}
