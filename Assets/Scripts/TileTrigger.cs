41f3a32acfb9db17b705fd8266aae2da8721778c
Jousset Nicolas <nicolas.jousset@gfi.fr>
2019-11-23 12:49:52 +0100

    Grid tiles

---
+++ Assets/Scripts/TileTrigger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileTrigger : MonoBehaviour
+{
+
+    public GameObject fireBallPreFab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    void OnTriggerStay2D(Collider2D col)
+    {
+
+        if (col.name == fireBallPreFab.name)
+        {
+            
+            print("Triger triggered");
+
+            print(col.name);
+        }
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.name == fireBallPreFab.name)
+        {
+
+            print("Triger triggered");
+
+            print(col.name);
+        }
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+    }
+}
